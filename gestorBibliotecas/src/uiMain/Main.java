c5a14681c7c70834b754c10b5eaccbe95db71af4
Auyazant <101429669+Auyazant@users.noreply.github.com>
2023-05-23 11:01:36 -0500

    fat

--- gestorBibliotecas/src/uiMain/Main.java
+++ gestorBibliotecas/src/uiMain/Main.java
@@ -831,7 +831,7 @@ public class Main {
 	            		libro.asignarLibro(user, biblioteca, bibliotecario);
 	            		user.hacerReporte();
 	            	}
-	            	
+					System.out.println("123");
 	            	
 	            		
 	            	
